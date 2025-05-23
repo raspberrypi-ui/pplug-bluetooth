f7629948a3919ae9e33baaffa4f04facbd562490
Simon Long <simon@raspberrypi.com>
2025-05-23 11:07:38 +0100

    Update translations

--- src/bluetooth.c
+++ src/bluetooth.c
@@ -2247,11 +2247,12 @@ static gboolean bluetooth_control (GtkWidget *plugin, const char *cmd)
     return bt_control_msg (bt, cmd);
 }
 
-FM_DEFINE_MODULE (lxpanel_gtk, bluetooth)
+int module_lxpanel_gtk_version = __FM_DEFINE_VERSION__(lxpanel_gtk);
+char module_name[] = PLUGIN_NAME;
 
 /* Plugin descriptor */
 LXPanelPluginInit fm_module_init_lxpanel_gtk = {
-    .name = N_(PLUGIN_TITLE),
+    .name = PLUGIN_TITLE,
     .description = N_("Manages Bluetooth devices"),
     .new_instance = bluetooth_constructor,
     .reconfigure = bluetooth_configuration_changed,
