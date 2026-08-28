6f528bd10c4f03ef3fff64330218d1ce3910381e
Simon Long <simon@raspberrypi.com>
2026-08-28 11:40:37 +0100

    Tidy shim

--- src/bluetooth_lxp.c
+++ src/bluetooth_lxp.c
@@ -73,11 +73,11 @@ char module_name[] = PLUGIN_NAME;
 /* Plugin descriptor */
 LXPanelPluginInit fm_module_init_lxpanel_gtk = {
     .name = PLUGIN_TITLE,
+    .gettext_package = GETTEXT_PACKAGE,
     .description = N_("Manages Bluetooth devices"),
     .new_instance = bluetooth_constructor,
     .reconfigure = bluetooth_configuration_changed,
-    .control = bluetooth_control,
-    .gettext_package = GETTEXT_PACKAGE
+    .control = bluetooth_control
 };
 
 /* End of file */
