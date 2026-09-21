6798af08bc1d9e20feecca0476b4e35b82f12db0
Simon Long <simon@raspberrypi.com>
2026-09-21 09:43:14 +0100

    Add tooltip for widget browser

--- src/bluetooth.h
+++ src/bluetooth.h
@@ -30,6 +30,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 /*----------------------------------------------------------------------------*/
 
 #define PLUGIN_TITLE N_("Bluetooth")
+#define PLUGIN_DESC N_("Manages Bluetooth devices")
 
 #define ICON_CACHE_SIZE 14
 
--- src/bluetooth_lxp.c
+++ src/bluetooth_lxp.c
@@ -73,8 +73,8 @@ char module_name[] = PLUGIN_NAME;
 /* Plugin descriptor */
 LXPanelPluginInit fm_module_init_lxpanel_gtk = {
     .name = PLUGIN_TITLE,
+    .description = PLUGIN_DESC,
     .gettext_package = GETTEXT_PACKAGE,
-    .description = N_("Manages Bluetooth devices"),
     .new_instance = bluetooth_constructor,
     .reconfigure = bluetooth_configuration_changed,
     .control = bluetooth_control
